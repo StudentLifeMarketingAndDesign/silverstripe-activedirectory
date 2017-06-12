5b18a34c26d0c7eead062dde999d9f2d488cb2a7
Dustin Quam <quamsta@gmail.com>
2017-06-12 14:55:35 -0500

    fix for baseurl. can't set manually for some reason

--- code/services/SAMLConfiguration.php
+++ code/services/SAMLConfiguration.php
@@ -46,7 +46,7 @@ class SAMLConfiguration extends Object
         $sp = $this->config()->get('SP');
 
         // set baseurl for SAML messages coming back to the SP
-        $conf['baseurl'] = $sp['entityId'];
+       // $conf['baseurl'] = Director::absoluteBaseURL(). 'saml/acs';
 
         $spCertPath = Director::is_absolute($sp['x509cert']) ? $sp['x509cert'] : sprintf('%s/%s', BASE_PATH, $sp['x509cert']);
         $spKeyPath = Director::is_absolute($sp['privateKey']) ? $sp['privateKey'] : sprintf('%s/%s', BASE_PATH, $sp['privateKey']);
